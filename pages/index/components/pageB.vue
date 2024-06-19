5b573d0cd953ead31a91f480a001c89062182026
baobao <1461471243@qq.com>
2024-06-20 00:51:11 +0800

    首次提交

---
+++ pages/index/components/pageB.vue
@@ -0,0 +1,20 @@
+<template>
+	<scroll-view style="height: 100%;" scroll-y="true" >
+		<view>
+			发现
+		</view>
+	</scroll-view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style>
+</style> 
---
+++ pages/index/components/pageC.vue
@@ -0,0 +1,20 @@
+<template>
+	<scroll-view style="height: 100%;" scroll-y="true" >
+		<view>
+			个人中心
+		</view>
+	</scroll-view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style>
+</style> 
